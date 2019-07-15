dde5c8919307026fc64bbeb55abbb8693734a713
Joao Francisco Medeiros Neto <joaom@weg.net>
2019-07-15 10:53:04 -0300

    automatized process of generating setup files depending of the version stored in package.json

--- setup/French Aggregator Pre Setup.bat
+++ setup/French Aggregator Pre Setup.bat
@@ -1,21 +1,36 @@
-SET version=1.0.1
 
+
+ECHO Get version of package.json
+@ECHO OFF
+FOR /f "usebackqdelims=" %%A IN (
+  `Powershell.exe -Nop -C "(Get-Content ..\package.json|ConvertFrom-Json).version"`
+) DO SET version=%%A
+ECHO AppVersion=%version%
+@ECHO ON
+
+ECHO Create new Inno Setup File
+SET issoutfile="French Aggregator %version%.iss"
+DEL %issoutfile%
+SET pscommand="(gc 'French Aggregator.tmpl') -replace 'OldAppVersion', '%version%' | Out-File -encoding ASCII '%issoutfile%'"
+powershell -Command %pscommand%
+pause
+
+ECHO Go and store App dir
 CD "..\"
 SET appdir=%CD%
 
+ECHO Go to Work dir
 CD "..\"
-CD
 
 ECHO Remove Dirs
-
-RD /S /Q "French Aggregator %version%-win32-x64"
-RD /S /Q "French Aggregator Setup %version%
+RD /S /Q "French Aggregator-win32-x64"
+RD /S /Q "French Aggregator Setup
 
 pause
 
-ECHO Electron-packager
+ECHO Run electron-packager
 REM --asar
-start electron-packager "%appdir%" "French Aggregator %version%" --platform=win32 --arch=x64 --electron-version=5.0.6 --overwrite --no-prune --ignore="setup" --ignore=".vscode" --asar
+start electron-packager "%appdir%" "French Aggregator" --platform=win32 --arch=x64 --electron-version=5.0.6 --overwrite --no-prune --ignore="setup" --ignore=".vscode" --asar
 
 pause
 
